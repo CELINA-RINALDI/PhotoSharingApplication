140d70d962fa170cacf4ccf72ed433843bc591bc
CELINA RINALDI <44110736@comunidadort.edu.ar>
2021-11-08 17:08:07 -0300

    Con vistas

--- Models/PhotoSharingInitializer.cs
+++ Models/PhotoSharingInitializer.cs
@@ -10,6 +10,68 @@ namespace PhotoSharingApplication.Models
 {
     public class PhotoSharingInitializer : DropCreateDatabaseAlways<PhotoSharingContext>
     {
+        //This method puts sample data into the database
+        protected override void Seed(PhotoSharingContext context)
+        {
+            base.Seed(context);
+
+            //Create some photos
+            var photos = new List<Photo>
+            {
+                new Photo {
+                    Title = "Me standing on top of a mountain",
+                    Description = "I was very impressed with myself",
+                    UserName = "Fred",
+                    PhotoFile = getFileBytes("\\Images\\flower.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    CreatedDate = DateTime.Today
+                },
+                new Photo {
+                    Title = "My New Adventure Works Bike",
+                    Description = "It's the bees knees!",
+                    UserName = "Fred",
+                    PhotoFile = getFileBytes("\\Images\\orchard.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    CreatedDate = DateTime.Today
+                },
+                new Photo {
+                    Title = "View from the start line",
+                    Description = "I took this photo just before we started over my handle bars.",
+                    UserName = "Sue",
+                    PhotoFile = getFileBytes("\\Images\\path.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    CreatedDate = DateTime.Today
+                }
+            };
+            photos.ForEach(s => context.Photos.Add(s));
+            context.SaveChanges();
+
+            //Create some comments
+            var comments = new List<Comment>
+            {
+                new Comment {
+                    PhotoID = 1,
+                    UserName = "Bert",
+                    Subject = "A Big Mountain",
+                    Body = "That looks like a very high mountain you have climbed"
+                },
+                new Comment {
+                    PhotoID = 1,
+                    UserName = "Sue",
+                    Subject = "So?",
+                    Body = "I climbed a mountain that high before breakfast everyday"
+                },
+                new Comment {
+                    PhotoID = 2,
+                    UserName = "Fred",
+                    Subject = "Jealous",
+                    Body = "Wow, that new bike looks great!"
+                }
+            };
+            comments.ForEach(s => context.Comments.Add(s));
+            context.SaveChanges();
+        }
+
         //This gets a byte array for a file at the path specified
         //The path is relative to the route of the web site
         //It is used to seed images
@@ -23,34 +85,6 @@ namespace PhotoSharingApplication.Models
             }
             return fileBytes;
         }
-        protected override void Seed(PhotoSharingContext context)
-        {
-            var photos = new List<Photo> {
 
-                  new Photo {
-               Title = "Test Photo",
-              Description = "Your Description",
-              UserName = "NaokiSato",
-             PhotoFile = getFileBytes("\\Images\\flower.jpg"),
-              ImageMimeType ="image/jpeg",
-                CreatedDate = DateTime.Today
-                 }
-                  };
-            photos.ForEach(s => context.Photos.Add(s));
-            context.SaveChanges();
-
-            var comments = new List<Comment> {
-                new Comment {
-                    PhotoID = 1,
-                    UserName = "NaokiSato",
-                    Subject = "Test Comment",
-                    Body = "This comment " + "should appear in " + "photo 1"
-                      }
-                        };
-            comments.ForEach(s => context.Comments.Add(s));
-            context.SaveChanges();
-
-
-        }
     }
 }
