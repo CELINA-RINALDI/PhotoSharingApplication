d94924bcacf300f887fd4b2e105b47d58cbd4ed3
CELINA RINALDI <44110736@comunidadort.edu.ar>
2021-11-08 16:03:00 -0300

    Agregar archivos de proyecto.

---
+++ Models/PhotoSharingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingContext: DbContext
+    {
+        public PhotoSharingContext() :base("PhotoSharingDB") { }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
